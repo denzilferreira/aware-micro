caebbb0acd3f4959548f12f51c9975ea1d63de00
Joaquín Pérez <joaquin@nurelm.com>
2019-10-22 15:01:00 -0300

    Merged with master. Added local DB support.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 /aware-micro/generate_aware_config.sh
 /aware-micro/gradlew $@
--- generate_aware_config.sh
+++ generate_aware_config.sh
@@ -2,9 +2,12 @@
 
 cat aware-config.json.template | \
   sed "s/%DB_ENGINE%/$DB_ENGINE/" | \
+  sed "s/%DB_HOST%/$DB_HOST/" | \
   sed "s/%DB_NAME%/$DB_NAME/" | \
   sed "s/%DB_USER%/$DB_USER/" | \
   sed "s/%DB_PWD%/$DB_PWD/" | \
   sed "s/%DB_PORT%/$DB_PORT/" | \
-  sed "s/%SERVER_DOMAIN%/$SERVER_DOMAIN/" | \
-  sed "s/%API_PORT%/$API_PORT/" > aware-config.json
+  sed "s|%SERVER_HOST%|$SERVER_HOST|" | \
+  sed "s/%SERVER_PORT%/$SERVER_PORT/" | \
+  sed "s/%WEBSOCKET_PORT%/$WEBSOCKET_PORT/" | \
+  sed "s/%STUDY_START%/$STUDY_START/" > aware-config.json
