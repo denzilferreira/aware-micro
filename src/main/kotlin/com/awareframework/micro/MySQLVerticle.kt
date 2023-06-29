17f893c493c1bdd80b19dbfd018fe9e6bb876587
Dai MIKURUBE <dmikurube@acm.org>
2023-06-29 10:59:13 +0900

    Ignore insertData when data is empty

--- src/main/kotlin/com/awareframework/micro/MySQLVerticle.kt
+++ src/main/kotlin/com/awareframework/micro/MySQLVerticle.kt
@@ -218,6 +218,10 @@ class MySQLVerticle : AbstractVerticle() {
    * Insert batch of data into database table
    */
   fun insertData(table: String, device_id: String, data: JsonArray) {
+    if (data.size() <= 0) {
+      return
+    }
+
     createTable(table)
       .onSuccess { _ ->
         sqlClient.getConnection { connectionResult ->
